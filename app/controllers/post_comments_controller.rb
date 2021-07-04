8bb4d6a0c462f96f188c4818d34c9720494a4484
pandaulait <show.hey.panda@gmail.com>
2021-07-04 08:11:42 +0000

    [Update]いいね機能コメント機能の非同期通信化

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,14 +1,17 @@
 class PostCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment =current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    @post_comment=PostComment.new
+
   end
   def destroy
+
     PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @post_comment=PostComment.new
   end
   private
 
